6b1c0e840b7e1b8b0a5746444854d7de4cdd27b5
Erdinç YAŞAN <erdincyasan97@hotmail.com>
2021-12-10 14:55:37 +0300

    routes configured

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\AppointmentsController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ContactsController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +18,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post("login",[AuthController::class,"login"]);
+Route::post("register",[AuthController::class,"register"]);
+Route::middleware("auth:sanctum")->group(function () {
+    Route::resource('users',UserController::class);
+    Route::resource('appointment', AppointmentsController::class);
+    Route::resource("contacts",ContactsController::class);
 });
